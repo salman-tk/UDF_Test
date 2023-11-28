91ee7aec6648cb8076229a59861e27af2067d500
salman-tk <112715650+salman-tk@users.noreply.github.com>
2023-11-28 10:25:50 +0000

    Update ExprFunctions.hpp

--- UDF/ExprFunctions.hpp
+++ UDF/ExprFunctions.hpp
@@ -76,12 +76,6 @@ inline int64_t round_threshold(float num_to_round, float threshold) {
     return return_num;
   }
 
-inline float multiply(float a, float b) {
-    float result;
-    result = a*b;
-    return result;
-  }
-  
 inline Assignment_Tuple2 Driver_PTA(
     int64_t clock_8_h,
     int64_t clock_11_h,
